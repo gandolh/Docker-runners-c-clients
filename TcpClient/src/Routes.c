9496df08b3bb0e740e46b58b192e003f47992f9f
gandolh <29356778+gandolh@users.noreply.github.com>
2024-04-17 00:00:29 +0300

    iterative search for routes in tree

--- TcpClient/src/Routes.c
+++ TcpClient/src/Routes.c
@@ -3,8 +3,9 @@
 #include <string.h>
 #include <stdio.h>
 
-struct Route * initRoute(char* key, char* value) {
-	struct Route * temp = (struct Route *) malloc(sizeof(struct Route));
+struct Route *initRoute(char *key, char *value)
+{
+	struct Route *temp = (struct Route *)malloc(sizeof(struct Route));
 
 	temp->key = key;
 	temp->value = value;
@@ -13,41 +14,55 @@ struct Route * initRoute(char* key, char* value) {
 	return temp;
 }
 
-void inorder(struct Route* root)
+void inorder(struct Route *root)
 {
 
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%s -> %s \n", root->key, root->value);
-        inorder(root->right);
-    }
+	if (root != NULL)
+	{
+		inorder(root->left);
+		printf("%s -> %s \n", root->key, root->value);
+		inorder(root->right);
+	}
 }
 
-void addRoute(struct Route ** root, char* key, char* value) {
-	if (*root == NULL) {
+void addRoute(struct Route **root, char *key, char *value)
+{
+	if (*root == NULL)
+	{
 		*root = initRoute(key, value);
 	}
-	else if (strcmp(key, (*root)->key) == 0) {
+	else if (strcmp(key, (*root)->key) == 0)
+	{
 		printf("============ WARNING ============\n");
 		printf("A Route For \"%s\" Already Exists\n", key);
-	}else if (strcmp(key, (*root)->key) > 0) {
+	}
+	else if (strcmp(key, (*root)->key) > 0)
+	{
 		addRoute(&(*root)->right, key, value);
-	}else {
+	}
+	else
+	{
 		addRoute(&(*root)->left, key, value);
 	}
 }
 
-struct Route * search(struct Route * root, char* key) {
-	if (root == NULL) {
-		return NULL;
-	} 
-
-	if (strcmp(key, root->key) == 0){
-		return root;
-	}else if (strcmp(key, root->key) > 0) {
-		return search(root->right, key);
-	}else if (strcmp(key, root->key) < 0) {
-		return search(root->left, key);
-	}  
-
+struct Route *search(struct Route *root, char *key)
+{
+	struct Route *selected = root;
+	while (selected != NULL)
+	{
+		if (strcmp(key, selected->key) == 0)
+		{
+			return selected;
+		}
+		else if (strcmp(key, selected->key) > 0)
+		{
+			selected = selected->right;
+		}
+		else if (strcmp(key, selected->key) < 0)
+		{
+			selected = selected->left;
+		}
+	}
+	return NULL;
 }
